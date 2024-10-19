900c7836f5caca855f144dda9d173b9dff9759a8
Aveesha Vivek <vivekbandaraofficial@gmail.com>
2024-10-20 00:20:25 +0530

    Add files via upload

---
+++ matrix effects 2.bat
@@ -0,0 +1,8 @@
+@echo off 
+pause
+color 0a
+title ------ Matrix Effect By Hacking Tube ---------
+mode 1000
+:top
+echo %random%  %random%  %random%  %random% %random%  %random%  %random%  %random% %random%  %random%  %random%  %random% %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random% %random%  %random%  %random%  %random%  %random%  %random%  %random%
+goto top
